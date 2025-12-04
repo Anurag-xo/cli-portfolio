763e8a595d5ac537985c47f43bbcd211799c663b
Anurag-xo <amnrag889@gmail.com>
2025-12-04 10:29:20 +0000

    fix(projects): correct import path for ProjectViewer

--- src/commands/projects.tsx
+++ src/commands/projects.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ProjectViewer } from '../../components/ProjectViewer';
+import { ProjectViewer } from '../components/ProjectViewer';
 
 interface Project {
   name: string;
