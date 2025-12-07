2419b1d326b3df78b8b9c2cdc3aa6a2c81f8213a
Anurag-xo <amnrag889@gmail.com>
2025-12-07 09:48:19 +0000

    feat(add): command history in clear

--- src/commands/clear.tsx
+++ src/commands/clear.tsx
@@ -1,5 +1,10 @@
-export const clear = (
-  setCommandHistory: React.Dispatch<React.SetStateAction<{ input: string; output: React.ReactNode }[]>>
-): void => {
-  setCommandHistory([]);
+import { ICommand } from '../types';
+
+export const clear: ICommand = {
+  name: 'clear',
+  description: 'Clears the terminal history.',
+  execute: (args, setCommandHistory) => {
+    setCommandHistory([]);
+    return '';
+  },
 };
