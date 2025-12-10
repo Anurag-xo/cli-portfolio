b1a72b1caf9c78ab54666e8013b400dc11d888c8
Anurag-xo <amnrag889@gmail.com>
2025-12-10 17:43:10 +0000

    feat(dynamic): clear commnad

--- src/commands/clear.tsx
+++ src/commands/clear.tsx
@@ -3,6 +3,7 @@ import { ICommand } from "../types";
 export const clear: ICommand = {
   name: "clear",
   description: "Clears the terminal history.",
+  category: 'General',
   execute: (args, setCommandHistory) => {
     setCommandHistory([]);
     return "";
